9f1cb466ada72082280dd71fa96e98a7bd2abae1
raymond-chao <raymond_chao45@hotmail.com>
2025-03-17 18:51:19 +0100

    js exercises done

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ console.log("Hello World");
 
 let name;
 name = "Raymond";
-console.log(name);
+console.log(`Your name is ${name}`);
 
 let age;
 age = 25;
-console.log(age);
+console.log(`Your age is ${age}`);
 
 let color;
 color = "blue";
-console.log(color);
+console.log(`Your favorite color is ${color}`);
+
+let num1;
+let num2;
+num1 = 10;
+num2 = 5;
+console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
+console.log(`The difference of ${num1} and ${num2} is ${num1 - num2}`);
+console.log(`The product of ${num1} and ${num2} is ${num1 * num2}`);
+console.log(`The quotient of ${num1} and ${num2} is ${num1 / num2}`);
